848c877bd955c1d03cbdbe9443ceddcd3cd4c8bb
Manu Gavilan <manuel.gavilan@saptools.es>
2021-01-14 21:37:21 +0100

    Added Exam Mode 1

--- app/src/main/java/saptools/mgavilan/nihongo/data/Question.kt
+++ app/src/main/java/saptools/mgavilan/nihongo/data/Question.kt
@@ -4,5 +4,5 @@ data class Question(
     val question: String ?= null,
     val answers: ArrayList<String> ?= null,
     val correctAnswer: Int = -1,
-    val selectedAnswer: Int = -1
+    var selectedAnswer: Int = -1
 )
--- app/src/main/java/saptools/mgavilan/nihongo/fragments/Exam1Fragment.kt
+++ app/src/main/java/saptools/mgavilan/nihongo/fragments/Exam1Fragment.kt
@@ -1,10 +1,15 @@
 package saptools.mgavilan.nihongo.fragments
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import saptools.mgavilan.nihongo.R
 import saptools.mgavilan.nihongo.data.KanjiItem
 import saptools.mgavilan.nihongo.data.Question
@@ -22,6 +27,10 @@ private const val ARG_PARAM2 = "param2"
 class Exam1Fragment(questionList: ArrayList<Question>) : Fragment() {
 
     private var questions = ArrayList<Question>()
+    var rootView: View? = null
+    var questionLL: LinearLayout ?= null
+    var answerLL: LinearLayout ?= null
+    var currentQuestion: Int = 0
 
     init {
         this.questions = questionList
@@ -36,6 +45,65 @@ class Exam1Fragment(questionList: ArrayList<Question>) : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exam1, container, false)
+        rootView = inflater.inflate(R.layout.fragment_exam1, container, false)
+
+        questionLL = rootView!!.findViewById(R.id.question)
+        answerLL = rootView!!.findViewById(R.id.answers)
+        generateQuestion(questions[currentQuestion])
+        return rootView
     }
+
+    /**
+     * Fills the upper part of the screen with the question and the lower part with the answers
+     */
+    private fun generateQuestion( question: Question ) {
+        questionLL!!.removeAllViewsInLayout()
+        answerLL!!.removeAllViewsInLayout()
+
+        // Generating Question TextView
+        val questionTV = TextView(requireActivity())
+        questionTV.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        questionTV.textSize = 80f
+        questionTV.typeface = Typeface.DEFAULT_BOLD
+        questionTV.text = question.question
+        questionTV.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+        questionTV.gravity = Gravity.CENTER or Gravity.CENTER_VERTICAL or Gravity.CENTER_HORIZONTAL
+
+        questionLL!!.addView(questionTV)
+
+        for ( i in 0 until question.answers!!.size) {
+            val cardView = CardView(requireActivity())
+            cardView.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                0,
+                1f
+            )
+            cardView.setPadding(16, 16, 16, 16)
+            cardView.setOnClickListener {
+                question.selectedAnswer = i
+                if ( currentQuestion < questions.size - 1 ) {
+                    currentQuestion++
+                    generateQuestion( questions[currentQuestion] )
+                }
+            }
+
+            val answerTV = TextView(requireActivity())
+            answerTV.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1f
+            )
+            answerTV.textSize = 20f
+            answerTV.text = question.answers[i]
+            answerTV.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+            answerTV.gravity = Gravity.CENTER or Gravity.CENTER_VERTICAL or Gravity.CENTER_HORIZONTAL
+
+            cardView.addView(answerTV)
+            answerLL!!.addView(cardView)
+        }
+    }
+
 }
